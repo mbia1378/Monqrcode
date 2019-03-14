d911a651333607600aa2d3efa6c17d8871e8da95
Cyrille MBIA <cyrille@itdreamreal.com>
2019-03-14 18:16:22 +0100

    Création des fichiers SpashActivity.java, spinner_item_color_selector.xml, activity_splash.xml, file_provider_paths.xml, modifications des fichiers strings.xml, AndroidManifest.xml, build.gradle

---
+++ app/src/main/java/com/mbia/cyrille/monqrcode/ui/splash/SplashActivity.java
@@ -0,0 +1,16 @@
+package com.mbia.cyrille.monqrcode.ui.splash;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.mbia.cyrille.monqrcode.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
